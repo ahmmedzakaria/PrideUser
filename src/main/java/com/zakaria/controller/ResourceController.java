57e00124caf8a1db67362a2f8451c785429d3d68
Zakaria Ahmmed <zakaria.com>
2020-11-29 19:49:08 +0600

    role problem fixed in api

--- src/main/java/com/zakaria/controller/ResourceController.java
+++ src/main/java/com/zakaria/controller/ResourceController.java
@@ -1,5 +1,10 @@
 package com.zakaria.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,15 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ResourceController {
 	
 	@RequestMapping("/user")
-	public String getUser()
+	public ResponseEntity<Map<String, Object>>  getUser()
 	{
-		return "Hello User";
+		Map<String,Object> data = new HashMap<>();	
+		data.put("role","user");
+		System.out.println("user=========================");
+		return new ResponseEntity<Map<String,Object>>(data, HttpStatus.OK);
 	}
 	
 	@RequestMapping("/admin")
 	public String getAdmin()
 	{
-		return "Hello Admin";
+		return "Admin";
 	}
 
 }
